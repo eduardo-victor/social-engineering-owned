80a7d247372f9505db2e0756e361c15efa2389ab
eduardo-victor <victorofish@gmail.com>
2024-04-11 17:19:05 -0300

    feat: added more one text

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
         <h1 className='text-slate-100 font-medium text-4xl font-pixel'>Did I Leak? D:</h1>
         <Form/>
         <p className='text-slate-50 font-semibold text-sm'>This site consults several dbleaks to check if your password has been leaked</p>
+        <p className='text-slate-50 font-semibold text-sm flex-wrap w-[70%]'>Pwned Passwords are hundreds of millions of real world passwords previously exposed in data breaches. This exposure makes them unsuitable for ongoing use as they're at much greater risk of being used to take over other accounts. They're searchable online below as well as being downloadable for use in other online systems. <a href="https://github.com/eduardo-victor/social-engineering-owned" className='border-b'>Read more about how HIBP protects the privacy of searched passwords</a>.</p>
       </div>
       </div>
     </div>
